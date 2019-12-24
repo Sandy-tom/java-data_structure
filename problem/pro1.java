edcc3b9e03d8cdaedf2ea1e8408b00b7a7ec52a8
Santhosh <49803785+Sandy-tom@users.noreply.github.com>
2019-12-24 23:27:03 +0530

    Update pro1.java
    solved full program in the page

--- problem/pro1.java
+++ problem/pro1.java
@@ -1,1 +1,44 @@
+import java.util.Scanner;
 
+public class swap1 {
+      
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner n = new Scanner(System.in);
+		int o = 0 ;
+          System.out.println("enter the value ");
+          int m = n.nextInt();
+          System.out.println("enter the operation value");
+          int v = n.nextInt();
+          if(v < 0) {
+        	  
+        	  o = m +v;
+        	  for(int i = 1;i<=m;i++) {
+        	     if(i<=o) {
+        		  System.out.print(i);
+        	     }
+        	     else {
+        	    	 System.out.print(i);
+        	    	 System.out.print(i);
+        	     }
+        	  }
+          }
+          else {
+        	  
+        	  for(int i = 1;i<=m;i++) {
+        		  
+        		  if(i<=v) {
+        			  System.out.print(i);
+        			  System.out.print(i);
+        		  }
+        		  else {
+        			  System.out.print(i);
+        		  }
+        	  }
+          // below it was the out put of the the program  
+/*enter the value 
+10
+enter the operation value
+-5
+12345667788991010 */
+            
